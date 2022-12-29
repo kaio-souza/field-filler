cc9ec55eaf04c9f294adde550a83bd9b84e6d790
Kaio Souza <kaio@hellosolucoes.com.br>
2022-12-29 20:29:57 -0300

    get presset

--- assets/js/background.js
+++ assets/js/background.js
@@ -4,30 +4,96 @@ chrome.runtime.onInstalled.addListener(function () {
     });
 });
 
+chrome.runtime.onInstalled.addListener(function () {
+    chrome.storage.sync.set({fillPreset: []}, function () {
+        console.log("[AAS] Fill Preset Created");
+    });
+});
+
 function listener(request, sender, sendResponse) {
     chrome.windows.getCurrent(function (currentWindow) {
         chrome.tabs.query({active: true, windowId: currentWindow.id}, function (activeTabs) {
             activeTabs.map(function (tab) {
-                chrome.scripting.executeScript({target: {tabId: tab.id}, func: fill,
-                    args: [request],}, function () {
-                    //chrome.scripting.executeScript({ target: { tabId: tabs[0].id }, files: ['Ruler.js'] });
-                });
+                if (request.type === 'fill') {
+                    chrome.scripting.executeScript({
+                        target: {tabId: tab.id}, func: fill,
+                        args: [request.data],
+                    }, function () {
+                        //chrome.scripting.executeScript({ target: { tabId: tabs[0].id }, files: ['Ruler.js'] });
+                    });
+                } else if (request.type === 'get') {
+
+                    chrome.scripting.executeScript({
+                        target: {tabId: tab.id}, func: getAllInputs,
+                        args: [],
+                    }, function (response) {
+                        let presets = [];
+                        chrome.storage.sync.get('fillPreset', function (data) {
+                            presets = data.fillPreset;
+                        });
+                        presets.push({
+                            name: request.data,
+                            preset: response[0].result
+                        });
+                        console.log(presets)
+
+                        chrome.storage.sync.set({fillPreset: presets}, function () {
+
+                        });
+                    });
+                }
             });
         });
     });
-
-
-    sendResponse([request])
+    sendResponse([]);
 }
 
 chrome.runtime.onMessage.addListener(listener)
 
 function fill(fillData) {
     fillData.forEach(function (item, index) {
-        var els = document.getElementsByName(item.name);
-        for (var i = 0; i < els.length; i++) {
+        let els = document.getElementsByName(item.name);
+        for (let i = 0; i < els.length; i++) {
+            let nativeInputValueSetter = Object.getOwnPropertyDescriptor(
+                window.HTMLInputElement.prototype,
+                "value"
+            ).set;
+            nativeInputValueSetter.call(els[i], item.value);
+            let inputEvent = new Event("input", {bubbles: true});
+            els[i].dispatchEvent(inputEvent);
+
             els[i].value = item.value;
         }
-console.log('adicionado')
+        console.log('adicionado')
     })
+}
+
+
+function getAllInputs() {
+    let inputs, selects, index, list = [];
+
+    inputs = document.getElementsByTagName('input');
+    for (index = 0; index < inputs.length; ++index) {
+        if (inputs[index].name.length > 0 && inputs[index].value.length > 0) {
+            list.push({
+                name: inputs[index].name,
+                value: inputs[index].value
+            })
+        }
+    }
+
+    selects = document.getElementsByTagName('select');
+    for (index = 0; index < selects.length; ++index) {
+        if (selects[index].name.length > 0 && selects[index].value.length > 0) {
+            list.push({
+                name: selects[index].name,
+                value: selects[index].value
+            })
+        }
+    }
+
+    console.log('listou')
+
+    return list;
+
 }
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,12 @@
 let fillData = [];
+let presetData = [];
 
 chrome.storage.sync.get('aac', function (data) {
     fillData = data.aac;
+});
+
+chrome.storage.sync.get('fillPreset', function (data) {
+    presetData = data.fillPreset;
 });
 
 document.addEventListener('DOMContentLoaded', function () {
@@ -9,11 +14,18 @@ document.addEventListener('DOMContentLoaded', function () {
 }, false);
 
 document.addEventListener('click', function (event) {
-
     event.preventDefault();
+
+    let message = {
+            type: 'fill',
+            data: fillData
+        },
+        fillName = document.querySelector('#fillName'),
+        fillValue = document.querySelector('#fillValue'),
+        fillPresetName = document.querySelector('#pressetName');
+
     if (event.target.matches('.add')) {
-        let fillName = document.querySelector('#fillName'),
-            fillValue = document.querySelector('#fillValue');
+
 
         if (fillName.value.length === 0 || fillValue.value.length === 0) {
             return;
@@ -37,10 +49,47 @@ document.addEventListener('click', function (event) {
         chrome.storage.sync.set({aac: fillData}, function () {
             listData();
         });
+    } else if (event.target.matches('.remove-preset')) {
+
+        presetData.splice(event.target.dataset.key, 1);
+        chrome.storage.sync.set({fillPreset: presetData}, function () {
+
+        });
+        listData();
+    } else if (event.target.matches('.use-preset')) {
+
+        fillData = presetData[event.target.dataset.key].preset;
+        chrome.storage.sync.set({aac: fillData}, function () {
+        });
+        listData();
+
     } else if (event.target.matches('.fill')) {
-        chrome.runtime.sendMessage(fillData, function (data) {
+        message.type = 'fill';
+        message.data = fillData;
+
+        chrome.runtime.sendMessage(message, function (data) {
             console.log(data);
         })
+
+    } else if (event.target.matches('.get')) {
+        message.type = 'get';
+        message.data = fillPresetName.value;
+
+        if (fillPresetName.value.length === 0) {
+            return;
+        }
+
+        chrome.runtime.sendMessage(message, function (data) {
+            console.log(data)
+        })
+
+        chrome.storage.sync.get('fillPreset', function (data) {
+            presetData = data.fillPreset;
+        });
+listData()
+
+        fillPresetName.value = '';
+        console.log(presetData)
 
 
     }
@@ -54,12 +103,23 @@ function listData() {
         text = '<table> <tr><th> Name</th> <th> Value</th> <th></th></tr>';
 
     fillData.forEach(function (item, key) {
-        text += '<tr><td>' + item.name + '</td><td>' + item.value + '</td> <td width="2"> <button class="remove" data-key="' + key + '">Remover</button></td> </tr>';
+        text += '<tr><td>' + item.name + '</td><td>' + item.value + '</td> <td width="2"> <button class="remove" data-key="' + key + '">X</button></td> </tr>';
     });
 
     text += '</table>';
 
     $container.innerHTML = fillData.length === 0 ? '( Empty )' : text;
+
+    let $containerPreset = document.querySelector('#pressets'),
+        textPreset = '<table> ';
+
+    presetData.forEach(function (item, key) {
+        textPreset += '<tr><td>' + item.name + '</td><td class="col-bt-preset">  <button class="use-preset" data-key="' + key + '">USE </button> <button class="remove-preset" data-key="' + key + '">X</button></td> </tr>';
+    });
+
+    textPreset += '</table>';
+
+    $containerPreset.innerHTML = presetData.length === 0 ? '( Empty )' : textPreset;
 }
 
 
